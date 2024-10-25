4ef3a294f8e05dfd67d111b793a7a6c4b8037f2b
elprofesorsunny <mohammad_khorshidi@outlook.fr>
2024-10-25 17:24:41 +0330

    feat: add theme store for dark/light mode management
    - Created a Zustand store with `isDarkMode` and `toggleDarkMode` state
    - Enables global dark/light mode toggle functionality

---
+++ website/src/store/useThemeStore.ts
@@ -0,0 +1,14 @@
+import { create } from 'zustand';
+
+interface ThemeState {
+    isDarkMode: boolean;
+    toggleDarkMode: () => void;
+}
+
+const useThemeStore = create<ThemeState>((set) => ({
+    isDarkMode: false,
+    toggleDarkMode: () =>
+        set((state) => ({ isDarkMode: !state.isDarkMode})), // Mode Change
+}))
+
+export default useThemeStore;
